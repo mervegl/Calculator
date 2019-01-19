538a887f2a34566311d4631966701a64836dbcf3
mervegl <munuremervegul@yahoo.com>
2019-01-19 14:34:53 -0500

    add comment to add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
 
+    // this is a very useful method
     public static double add(double num1, double num2){
         return num1 + num2;
     }
@@ -8,6 +9,7 @@ public class Calculator {
         return num1 - num2;
     }
 
-    public static double multiply(double num1, double num2){
+    public static double multiply(double num1, double num2) {
         return num1 * num2;
+    }
 }
